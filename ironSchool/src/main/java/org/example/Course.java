9aeb28015a07e2fc7d5d39a49d1070197edb70cd
nomadriding <fundora.a@gmail.com>
2024-06-26 12:15:37 -0400

    create course class, with getters and setters

---
+++ ironSchool/src/main/java/org/example/Course.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Course {
+    private String courseId;
+    private String name;
+    private double price;
+    private double money_earned;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMoney_earned() {
+        return money_earned;
+    }
+
+    public void setMoney_earned(double money_earned) {
+        this.money_earned = money_earned;
+    }
+}
