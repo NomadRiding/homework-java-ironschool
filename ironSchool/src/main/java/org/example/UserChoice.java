88db8db05cd18fb6e4881747a3edcfe697f16da7
nomadriding <fundora.a@gmail.com>
2024-06-26 10:04:28 -0400

    import scanner

--- ironSchool/src/main/java/org/example/UserChoice.java
+++ ironSchool/src/main/java/org/example/UserChoice.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class UserChoice {
+    private static Scanner scanner;
     public static void main(String[] args) {
         welcome();
     }
     public static void welcome(){
         System.out.println("Welcome to Advance Learning \n");
         System.out.println("Choose a name for this school: ");
+        schoolName();
+    }
+
+    public static void schoolName(){
 
     }
 
