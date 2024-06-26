e7b78dee7ef0b6204a2733d9bc6cd4f4d0bebec3
nomadriding <fundora.a@gmail.com>
2024-06-26 10:02:52 -0400

    Add jdk file, add welcome method

--- ironSchool/src/main/java/org/example/UserChoice.java
+++ ironSchool/src/main/java/org/example/UserChoice.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class UserChoice {
+    public static void main(String[] args) {
+        welcome();
+    }
+    public static void welcome(){
+        System.out.println("Welcome to Advance Learning \n");
+        System.out.println("Choose a name for this school: ");
+
+    }
+
 
 }
